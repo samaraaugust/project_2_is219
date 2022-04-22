d6255096bbc3bb643fe8a89ff16bbcd97ba464ec
samaraaugust <samaraaugust123@gmail.com>
2022-04-22 11:04:36 -0400

    created html page for upload and added to nav

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -1,4 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
 map = Blueprint('map', __name__,
                         template_folder='templates')
+
+@map.route('/upload')
+def upload():
+    try:
+        return render_template('upload_map.html')
+    except TemplateNotFound:
+        abort(404)
