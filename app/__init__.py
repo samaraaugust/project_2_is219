08cfa88a9079423ffc5159538beba3df5f5322ec
samaraaugust <samaraaugust123@gmail.com>
2022-04-30 19:11:41 -0400

    password validation for registration

--- app/__init__.py
+++ app/__init__.py
@@ -42,12 +42,13 @@ def create_app():
         app.config.from_object("app.config.DevelopmentConfig")
     elif os.environ.get("FLASK_ENV") == "testing":
         app.config.from_object("app.config.TestingConfig")
+        app.config['WTF_CSRF_ENABLED'] = False
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     csrf = CSRFProtect(app)
     #csrf.exempt(auth)
-    app.config['WTF_CSRF_ENABLED'] = False
+
     bootstrap = Bootstrap5(app)
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
