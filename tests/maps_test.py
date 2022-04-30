725b2bfaf81bf06ce7f40d3e3e94f7f0ba3f8903
samaraaugust <samaraaugust123@gmail.com>
2022-04-30 11:20:27 -0400

    added tests for uploading csv

--- tests/maps_test.py
+++ tests/maps_test.py
@@ -1,10 +1,10 @@
 def test_csv_upload(client, auth):
     response1 = auth.register()
     response3 = auth.login()
-    csv = "tests/test_music.csv"
+    csv = "tests/test_cities_short.csv"
     csv_data = open(csv, "rb")
-    data = {"file": (csv_data, "test_music.csv")}
-    response2 = client.post("/songs", data=data)
+    data = {"file": (csv_data, "test_cities_short.csv")}
+    response2 = client.post("/locations/upload", data=data)
     print(response2.data)
     assert response2.status_code == 302
-    assert response2.headers["Location"] == "/songs_tables"
+    assert response2.headers["Location"] == "/locations"
